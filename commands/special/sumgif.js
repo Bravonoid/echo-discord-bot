89b04e844f781a50b311494707b113a0786083f3
Diki Bagastama <dikibagas139@gmail.com>
2022-03-01 15:27:40 +0700

    polishing somethin sketchy

--- commands/special/sumgif.js
+++ commands/special/sumgif.js
@@ -1,17 +1,18 @@
 const { MessageEmbed } = require("discord.js");
 const client = require("nekos.life");
+const { color } = require("../../config.json");
 
 module.exports = {
-  name: "sumgif",
-  description: "Sends a sus neko image",
-  execute(msg) {
-    const neko = new client();
-    neko.nsfw.randomHentaiGif().then((neko) => {
-      const embed = new MessageEmbed()
-        .setTitle("Neko")
-        .setImage(neko.url)
-        .setColor("RANDOM");
-      msg.channel.send({ embeds: [embed] });
-    });
-  },
+	name: "sumgif",
+	description: "Sends a sus neko image",
+	execute(msg) {
+		const neko = new client();
+		neko.nsfw.randomHentaiGif().then((neko) => {
+			const embed = new MessageEmbed()
+				.setTitle("Neko")
+				.setImage(neko.url)
+				.setColor(color);
+			msg.channel.send({ embeds: [embed] });
+		});
+	},
 };
