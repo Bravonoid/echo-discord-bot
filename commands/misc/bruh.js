640fbc7a6b0b2e2821b4b5c4d7477169659e569e
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2022-03-01 14:07:15 +0700

    added the random function bruh

--- commands/misc/bruh.js
+++ commands/misc/bruh.js
@@ -2,18 +2,20 @@ const { MessageEmbed } = require("discord.js");
 const akaneko = require('akaneko');
 
 module.exports = {
-	name: "random",
+	name: "bruh",
 	description: "Get a special picture",
 	async execute(msg) {
 		if (!msg.channel.nsfw){
 			msg.channel.send('oof this channel is not safeeee')
 		}
-
-		const embed = new MessageEmbed()
+		else{
+			const embed = new MessageEmbed()
 			.setColor(color)
 			.setTitle(`Here's a COMPLETELY RANDOM SPECIAL PICTURE for you`)
 			.setImage(await akaneko.lewdNeko())
 
 		msg.channel.send({ embeds: [embed] });
+		}
+	
 	},
 };
